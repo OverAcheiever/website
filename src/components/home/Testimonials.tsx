2f107b31184eca5ed2899f27a3425ad7a93676ae
cryptopavelsan <greatapepavelsan@gmail.com>
2023-02-07 19:39:34 +0000

    Mud is cleared

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const Testimonials = () => {
   const testimonials = [
     {
@@ -20,17 +22,15 @@ const Testimonials = () => {
   return (
     <div className="flex w-full flex-col items-center bg-[url('/images/testimonials/bg.png')] bg-cover pt-40">
       <div className="text-center font-tt text-7xl ">
-        GET BETTER WITH <br /> FEEDBACK
+      GET BETTER WITH <br /> FEEDBACK
       </div>
-      
+      <div className="mt-10 text-center font-space text-xl">
         Dean&apos;s List is a Service DAO consisting of Web3 power users, <br />{" "}
         who provide valuable feedback sessions to projects
       </div>
-
       <div className="mt-10 text-left font-space text-xl" style="max-width:50%;">
-      The Dean&apos;s List feedback platform provides a reliable and trustworthy space for you to receive valuable insights and suggestions from a diverse community of experts and users. By leveraging the power of the Web3 ecosystem, you can be confident that your feedback is secure, transparent, and protected against tampering. Whether you are a developer, entrepreneur, or simply looking to improve your project, the Dean&apos;'s List provides the tools and resources you need to succeed. So why wait? <Link href="https://coinablepay.com/store/deans-list">Get started today</Link> and bring your project to the next level with the Dean&apos;s List!
+      The Dean&apos;s List feedback platform provides a reliable and trustworthy space for you to receive valuable insights and suggestions from a diverse community of experts and users. By leveraging the power of the Web3 ecosystem, you can be confident that your feedback is secure, transparent, and protected against tampering. Whether you are a developer, entrepreneur, or simply looking to improve your project, the Dean&apos;s List provides the tools and resources you need to succeed. So why wait? <Link href="https://coinablepay.com/store/deans-list">Get started today</Link> and bring your project to the next level with the Dean&apos;s List! <br />{" "}
       </div>
-
 
       <div className="grid grid-cols-3 gap-5 py-20 font-space">
         {testimonials.map((testimonial, key) => (
