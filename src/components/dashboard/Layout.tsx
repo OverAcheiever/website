971ca3cba7319976592c836f74836b7602c7426e
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-02-20 15:51:49 +0530

    Revert "added request feedback button to dashboard"
    This reverts commit 80f52ead5ef04bd5cd527e349f40ab7cad33088f.

--- src/components/dashboard/Layout.tsx
+++ src/components/dashboard/Layout.tsx
@@ -17,6 +17,11 @@ const Layout = ({ children }: { children: ReactNode }) => {
     <div className="relative flex min-h-screen w-full flex-col bg-black text-white">
       <div className="flex h-20 w-full items-center justify-between border-b border-[#111] px-8 font-tt text-3xl">
         DEAN&apos;S LIST
+        <Link href="/dashboard/create">
+          <button className="font-font-space h-10 rounded bg-white px-5 text-lg font-bold text-black">
+            create
+          </button>
+        </Link>
       </div>
       {!publicKey ? (
         <div className="flex w-full flex-grow items-center justify-center">
